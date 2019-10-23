0f7fe8555c754ed0a92a734f0360353a3e4fe2e2
Mukesh Gandhi <MukkeshGandhi09@users.noreply.github.com>
2019-10-23 07:49:20 +0530

    Position+Velocity initiated

--- Codes/Control/RMCS2303 control.ino
+++ Codes/Control/RMCS2303 control.ino
@@ -5,19 +5,25 @@ RMCS2303 rmcs;                    //object for class RMCS2303
 
 SoftwareSerial myserial(2,3);     //Software Serial port For Arduino Uno. Comment out if using Mega.
 
-
-byte slave_id=7;                  // Should be changed when 2 drivers are used for MODBUS protocol
+// MODBUS protocol for 2 slave - 1 master communication 
+//MOTOR 1 PARAMETERS
+byte slave_id=7; 
 int INP_CONTROL_MODE=203;           
 int PP_gain=32;
 int PI_gain=16;
 int VF_gain=32;
+//MOTOR 2 PARAMETERS
+byte slave_id1=8                 // Should be changed when 2 drivers are used for MODBUS protocol
+int PP_gain1=32;
+int PI_gain1=16;
+int VF_gain1=32;
 int LPR=334;
 int acceleration=5000;
 int speed=100;
 
 
-long int Current_position;
-long int Current_Speed;
+long int Current_position,Current_position1;
+long int Current_Speed,Current_Speed1;
 
 void setup()
 {
@@ -25,6 +31,7 @@ void setup()
    rmcs.Serial0(9600);             //Set baudrate for usb serial to monitor data on serial monitor
    rmcs.begin(&myserial,9600);     //Uncomment if using software serial port. Comment this line if using hardware serial.
    rmcs.WRITE_PARAMETER(slave_id,INP_CONTROL_MODE,PP_gain,PI_gain,VF_gain,LPR,acceleration,speed);    //Uncomment to write parameters to drive. Comment to ignore.
+   rmcs.WRITE_PARAMETER(slave_id1,INP_CONTROL_MODE,PP_gain1,PI_gain1,VF_gain1,LPR,acceleration,speed);
    //rmcs.READ_PARAMETER(slave_id);//DEBUG function
    
 }
@@ -33,29 +40,15 @@ void loop()
 {
   
    rmcs.Speed(slave_id,100);                   //Set speed within range of 0-65535 or 0-(maximum speed of base motor)
-   rmcs.Enable_Digital_Mode(slave_id,0);       //To enable motor in digital speed control mode. 0-fwd,1-reverse direction. 
-
-   delay(3000);
+   rmcs.Enable_Digital_Mode(slave_id,0);       //To enable motor in digital speed control mode. 0-fwd,1-reverse direction.
+   rmcs.Speed(slave_id1,100); 
+   rmcs.Enable_Digital_Mode(slave_id1,0);       //To enable motor in digital speed control mode. 0-fwd,1-reverse direction.
+   delay(100);
    Current_Speed=rmcs.Speed_Feedback(slave_id); 
+   Current_Speed1=rmcs.Speed_Feedback(slave_id1);
    Serial.print("Current Speed feedback : ");
    Serial.println(Current_Speed);
+   Serial.print("Current Speed feedback1 : ");
+   Serial.println(Current_Speed1);
 
-   delay(5000);
-   Serial.println("Break Motor");
-   rmcs.Brake_Motor(slave_id,0);                 //Brake motor. 0-fwd,1-reverse direction. 
-
-   delay(2000);
-   Serial.println("Changing direction");
-   rmcs.Enable_Digital_Mode(slave_id,1);           //To enable motor in digital speed control mode. 0-fwd,1-reverse direction. 
-
-   delay(3000);
-   Current_Speed=rmcs.Speed_Feedback(slave_id); 
-   Serial.print("Current Speed feedback : ");
-   Serial.println(Current_Speed);
-
-   
-   delay(3000);
-   Serial.println("Disable Motor");
-   rmcs.Disable_Digital_Mode(slave_id,1);        //Disable motor in digital speed control mode. 0-fwd,1-reverse direction. 
-   delay(3000);
 }
