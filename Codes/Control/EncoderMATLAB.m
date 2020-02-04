39a02386977dbd0282d0a314a97a862a5473e270
Mukesh Gandhi <MukkeshGandhi09@users.noreply.github.com>
2020-02-05 05:27:27 +0530

    Tuned system

--- Codes/Control/EncoderMATLAB.m
+++ Codes/Control/EncoderMATLAB.m
@@ -8,28 +8,38 @@ encoder = rotaryEncoder(a,'D2','D3',810);
 
 pwm='D5';
 direction='D7';
-kp=0.7;
-ki=0.3;
-kd=0.01;
+kp=0.09;
+ki=0.09;
+kd=0;
 error=0;
 current_val=0;
 error_prev=0;
 dt=0;
-setpoint=15000;
+setpoint=6000;
 prevtime=0;
 scale=100;
-
+iscale=1;
+risetime=0;
+settime=0;
 figure
 h=animatedline;
 ylabel("Response");
+ylim([0,8500]);
 
 while(1)
     [current_val,time]=readCount(encoder);
+    if(current_val==0.9*setpoint)
+        risetime=time;
+    end
+    
+    if(current_val>=0.80*setpoint & current_val <= 1.8*setpoint)
+        settime=time;
+    end
     dt=time-prevtime;
     error=setpoint-current_val;
     error_sum=(error_prev+error)*dt;
     error_diff=(error-error_prev)/dt;
-    pid=error*kp+error_diff*kd+error_sum*ki;
+    pid=error*kp+error_diff*kd+(error_sum*ki/iscale);
     pid=pid/scale;
     prevtime=time;
     error_prev=error;
@@ -48,7 +58,7 @@ while(1)
     end 
     
     addpoints(h,time,current_val);
-    xlim([time-10,time+10]);
+    xlim([time-50,time+50]);
     drawnow;
     
 end
